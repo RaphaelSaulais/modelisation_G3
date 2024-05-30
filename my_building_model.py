5d2d5ddcf07ed16785ca09d191a4bcd15af2528a
Godzeu75 <166369314+Godzeu75@users.noreply.github.com>
2024-05-30 14:27:37 +0200

    Update my_building_model.py

--- my_building_model.py
+++ my_building_model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 import dm4bem
 
-hauteur = 3 #hauteur 
+hauteur = 3 #hauteur appartment 
 L1 = 5 #largeur maison
 L2 =9 #longueur maison
 L3 = 2 #largeur petite pièce
